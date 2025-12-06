ef025771b7788f3957961d01122ee49716b5898d
MohdAlam <mdalam@zindagi.tech>
2025-12-06 12:46:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Arvion from "./pages/products/arvion";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/products-arvion/">
       <Routes>
         <Route path="/" element={<Arvion />} />
       </Routes>
